9911b736e9c301f3fb15522634b0ac14e0fac172
Fredrik Lundin <lundin_fredrik@hotmail.com>
2011-12-13 19:42:39 +0100

    fixat nån rad i centerpanel

--- Inlupp2_prog2_git/src/CenterPanel.java
+++ Inlupp2_prog2_git/src/CenterPanel.java
@@ -6,7 +6,7 @@ import javax.swing.JPanel;
 
 public class CenterPanel extends JPanel {
 	private Image bg;
-	private double width=1, height=1;
+	private double width=-1, height=1;
 
 	public CenterPanel() {
 		bg = null;
@@ -18,22 +18,31 @@ public class CenterPanel extends JPanel {
 	}
 
 	public int getImgWidth() {
+		System.out.println("getImgWidth: "+width);
 		return (int) width;
 	}
 
 	public int getImgHeight() {
+		System.out.println("getImgHeight: "+height);
 		return (int) height;
 	}
 
 	public void addImage(String img) {
 		setLayout(null);
-		bg = Toolkit.getDefaultToolkit().getImage(img);
-		width = bg.getWidth(this);
-		height = bg.getHeight(this);
+		bg = Toolkit.getDefaultToolkit().getImage(img).getScaledInstance(700, -1, 20); //skalar bilden till 700 i bredd, sista argumentet fattar jag inte, men blir nice med 20 :P
+		width = -1;
+		while(width == -1) //bilden laddar inte direkt, så måste vänta tills storleken inte är -1
+			width = bg.getWidth(this);
 		
-		double scale = width/height;
-		width = 700;
-		height = width/scale;
+		width = bg.getWidth(this);
+		System.out.println("bg.getWidth: "+bg.getWidth(this));
+		height = bg.getHeight(this);
+		System.out.println("bg.getHeight: "+bg.getHeight(this));
+		
+//		double scale = width/height;
+//		System.out.println("Scale: "+scale);
+//		width = 700;
+//		height = width/scale;
 
 	}
 }
